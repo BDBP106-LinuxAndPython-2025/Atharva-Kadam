2ab095a68ed57618e845c441ef113a10765ed617
Atharva-Kadam26 <atharvakadam262004@gmail.com>
2025-09-05 19:08:17 +0530

    Mock

--- IA-1_Practicals_MOCK/question2.sh
+++ IA-1_Practicals_MOCK/question2.sh
@@ -11,4 +11,4 @@ sort -k 2 -n pig_weight.csv -o col2_sorted.out
 sort -k 3 -n pig_weight.csv -o col3_sorted.out
 
 #sorting the pig_weights.csv by 2nd column
-sort -k 4 -n pig_weight.csv > col4_sorted.out
+sort -k 4 -n pig_weight.csv -o  col4_sorted.out
